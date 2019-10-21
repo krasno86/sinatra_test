59d563f88d3869c248751e7bf28a055633e97a59
oleg <krasno1986@gmail.com>
2019-10-21 20:08:49 +0300

    add file attachments

--- app.rb
+++ app.rb
@@ -2,29 +2,28 @@ require 'sinatra'
 require_relative 'models/message'
 require 'pony'
 require 'dotenv/load'
+require 'net/smtp'
 
 get '/messages/new' do
   erb :'/messages/new'
 end
 
 post '/messages/create' do
-  p params
-  # @message = Message.new(params)
-
-   Pony.mail({
-      from: params[:email],
-      to: ENV['ADMIN_EMAIL'],
-      subject: "user #{params[:name]} feedback",
-      body: params[:body],
-      via: :smtp,
-      via_options: {
-          address: 'smtp.gmail.com',
-          domain: 'gmail.com',
-          port: '587',
-          user_name: ENV['USERNAME'],
-          password: ENV['PASSWORD']
-        }
-    })
+  Pony.mail({
+    from: params[:email],
+    to: ENV['ADMIN_EMAIL'],
+    subject: "user #{params[:name]} feedback",
+    body: params[:body],
+    attachments: {params[:file][:filename] => File.read(params[:file][:tempfile])},
+    via: :smtp,
+    via_options: {
+      address: 'smtp.gmail.com',
+      domain: 'gmail.com',
+      port: '587',
+      user_name: ENV['USERNAME'],
+      password: ENV['PASSWORD']
+    }
+  })
 
   redirect '/messages/new'
 end
--- models/message.rb
+++
@@ -1,9 +0,0 @@
-class Message
-  attr_accessor :name, :email, :body
-
-  def initialize(params = {})
-    @name = params[:name]
-    @email = params[:email]
-    @body = params[:body]
-  end
-end
