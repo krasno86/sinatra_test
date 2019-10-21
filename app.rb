3b2bd0403bfb2db1ede0d4458fa2ccce179842f6
oleg <krasno1986@gmail.com>
2019-10-22 01:11:27 +0300

    add validator

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'rack/validator/sinatra'
 require 'sinatra'
 require 'pony'
 require 'dotenv/load'
@@ -7,8 +8,16 @@ get '/' do
 end
 
 post '/messages/create' do
-  if params[:name].empty? || params[:email].empty? || params[:body].empty?
-    redirect '/'
+  validator = Rack::Validator.new(params, true)
+  validator.required [:name, :email, :body]
+  validator.downcase :email
+  validator.is_in_range 10, 32, :name
+  validator.is_email :email
+  validator.matches /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :email
+  validator.is_greater_equal_than 50, :body
+
+  if validator.has_errors?
+    p validator.messages.join('|')
   else
     attachments = params[:file] ? {params[:file][:filename] => File.read(params[:file][:tempfile])} : ''
     Pony.mail({
