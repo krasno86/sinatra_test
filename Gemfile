100a2d15871aa214e42a00c1932d8e5af1bd64a7
oleg <krasno1986@gmail.com>
2019-10-21 15:42:20 +0300

    add dot env gem, pony mailer with smtp settings

--- Gemfile
+++ Gemfile
@@ -8,6 +8,10 @@ gem 'activesupport'
 gem 'shotgun'
 gem 'tux'
 gem 'rubocop', '~> 0.75.1', require: false
+gem 'sendgrid-ruby'
+gem 'pony', '~> 1.13', '>= 1.13.1'
+gem "letter_opener", :group => :development
+
 
 group :test do
  gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,10 +1,32 @@
 require 'sinatra'
+require 'sendgrid-ruby'
 require_relative 'models/message'
+require 'pony'
+require 'dotenv/load'
 
 get '/messages/new' do
   erb :'/messages/new'
 end
 
 post '/messages/create' do
-  @message = Message.new(params)
+  p '111111111111111'
+  p params
+  # @message = Message.new(params)
+
+     Pony.mail({
+        from: params[:email],
+        to: ENV['ADMIN_EMAIL'],
+        subject: "user #{params[:name]} feedback",
+        body: params[:body],
+        via: :smtp,
+        via_options: {
+            address: 'smtp.gmail.com',
+            domain: 'gmail.com',
+            port: '587',
+            user_name: ENV['USERNAME'],
+            password: ENV['PASSWORD']
+        }
+    })
+
+  redirect '/messages/new'
 end
